8474ac018d388fe79fa6a2bc58132ba2dd8c7234
Joonmo-dev <yunedoopea610@gmail.com>
2019-05-20 14:20:19 +0900

    Add tslint config

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+  return <div className="App" />;
+};
 
 export default App;
