76c5ff45098ddaf737460d08c797a50a75c79d69
khg0712 <k7120792@gmail.com>
2019-05-24 11:53:24 +0900

    Add ListItem

---
+++ src/presentations/ListItem/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import * as S from './styles';
+import { formatDate } from '../../utils';
+
+interface Props {
+  imageSrc: string;
+  itemName: string;
+  itemId: string;
+  ownerName: string;
+  sharedDate: Date;
+  returnDate: Date;
+  onClick?: () => void;
+}
+
+export const ListItem: React.FC<Props> = ({
+  imageSrc,
+  itemName,
+  itemId,
+  ownerName,
+  sharedDate,
+  returnDate,
+  onClick,
+}) => {
+  return (
+    <S.ListItem onClick={onClick}>
+      <S.ListItemImage src={imageSrc} />
+      <S.ListItemDescrition>
+        <S.ListItemDescritionLeft>
+          <S.ListItemName>{itemName}</S.ListItemName>
+          <S.ListItemOwner>{ownerName}</S.ListItemOwner>
+        </S.ListItemDescritionLeft>
+        <S.ListItemDateDescription>
+          {`${formatDate(sharedDate)} ~ ${formatDate(returnDate)}`}
+        </S.ListItemDateDescription>
+      </S.ListItemDescrition>
+    </S.ListItem>
+  );
+};
