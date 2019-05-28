e10cb3fabebc0fc2713fdf59df246c8b420d39cb
khg0712 <k7120792@gmail.com>
2019-05-28 10:28:24 +0900

    Modify props interface name

--- src/presentations/TypeFilter/styles.ts
+++ src/presentations/TypeFilter/styles.ts
@@ -1,31 +1,29 @@
 import styled from 'styled-components';
 
-interface TypeFilterProps {
-  isSelected: boolean;
-}
-
 export const TypeFilter = styled.div`
   position: relative;
   display: inline-block;
 `;
 
-interface DropdownbtnProps {}
+interface DropdownbtnProps {
+  isSelected: boolean;
+}
 
 export const Dropdownbtn = styled.div`
   border-radius: 4px;
   border: 1px solid;
-  border-color: ${({ isSelected }: TypeFilterProps) =>
+  border-color: ${({ isSelected }: DropdownbtnProps) =>
     isSelected ? '#008489' : '#dce0e0'};
   padding: 6px 12px;
-  background: ${({ isSelected }: TypeFilterProps) =>
+  background: ${({ isSelected }: DropdownbtnProps) =>
     isSelected ? '#008489' : '#fff'};
-  color: ${({ isSelected }: TypeFilterProps) =>
+  color: ${({ isSelected }: DropdownbtnProps) =>
     isSelected ? '#fff' : '#484848'};
   font-size: 14px;
   &:hover {
-    background: ${({ isSelected }: TypeFilterProps) =>
+    background: ${({ isSelected }: DropdownbtnProps) =>
       isSelected ? '#006c70' : '#f2f2f2'};
-    border-color: ${({ isSelected }: TypeFilterProps) =>
+    border-color: ${({ isSelected }: DropdownbtnProps) =>
       isSelected ? '#006c70' : '#f2f2f2'};
   }
 `;
