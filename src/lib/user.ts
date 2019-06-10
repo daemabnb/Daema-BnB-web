13ad499f6e7f6b2c5f4e773c5240afd1bb1f7dcc
김형규 <k7120792@gmail.com>
2019-06-10 15:52:28 +0900

    Add user api

---
+++ src/lib/user.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const baseUrl = 'https://52.79.121.254/user';
+
+export const authMail = (email: string) => axios.post(`${baseUrl}/authmail`, {
+  email,
+});
+
+export const signup = (email: string, authNum: string) => axios.post(`${baseUrl}/signup`, {
+  email,
+  authNum,
+});
+
+export const signin = () => axios.get(`${baseUrl}/signin/facebook`);
