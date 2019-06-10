54bcd6ee152cfa97d6cc407bc938596dbb85a9bb
김형규 <k7120792@gmail.com>
2019-06-10 19:06:03 +0900

    Add FacebookLogin to Signin page

--- src/containers/Signin.tsx
+++ src/containers/Signin.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
-import { Button } from '../presentations';
-import { signin } from '../lib/user';
+import FacebookLogin from 'react-facebook-login';
+// import { signin } from '../lib/user';
 
 interface Props {
 }
 
 export const Signin: React.FC = () => {
-  const onSignin = async () => {
-    const response = await signin();
-    console.log(response.data.token);
-  }
+  const responseFacebook = (response: any) => {
+    console.log(response);
+  };
+
   return (
     <div>
-      <Button content="Login with Facebook" size="big" onClick={onSignin} />
+      <FacebookLogin
+        appId="856575621215696"
+        autoLoad={true}
+        callback={responseFacebook}
+      />
     </div>
   );
 };
