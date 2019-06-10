c4886473f26c2a126fd70598b3d6ff7c2d48dfa1
김형규 <k7120792@gmail.com>
2019-06-10 15:54:04 +0900

    Add router to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import './App.css';
+import { Router, Route, Switch } from 'react-router';
+import { createBrowserHistory } from 'history';
+import { Signup, Signin } from './containers';
 
-const App: React.FC = () => {
-  return <div className="App" />;
-};
+interface Props {}
+
+interface State {
+}
+
+const history = createBrowserHistory();
+
+class App extends React.Component<Props, State> {
+  render() {
+    return (
+      <Router history={history}>
+        <Switch>
+          <Route path="/" component={Signup} />
+          <Route path="/signin" component={Signin} />
+        </Switch>
+      </Router>
+    );
+  }
+}
 
 export default App;
