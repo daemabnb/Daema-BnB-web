7bddf5807386a75a93e7d219b80c289458ab9e0c
Joonmo-dev <yunedoopea610@gmail.com>
2019-06-12 20:26:36 +0900

    Change settinig state idea
    onChange and onReducer are not support second callback argument
    So I used useCallback api and also use useReducer for visibility

--- src/presentations/TypeFilterDropdownButton/index.tsx
+++ src/presentations/TypeFilterDropdownButton/index.tsx
@@ -1,31 +1,59 @@
-import React, { FC } from 'react';
+import React, { FC, useReducer, useCallback } from 'react';
 import { DropdownButton } from '..';
 import * as S from './styles';
 
-type valueType = '공용' | '개인';
-
 interface TypeFilterDropdownButtonProps {
-  onChange: (value: valueType) => void;
-  value?: valueType;
-  onApply: () => void;
+  onApply: (value: null | '공용' | '개인') => void;
 }
 
+interface TypeFilterDropdownButtonState {
+  type: null | '공용' | '개인';
+}
+
+type ActionType = {
+  type: 'set personal' | 'set public';
+};
+
+const reducer = (
+  state: TypeFilterDropdownButtonState,
+  action: ActionType,
+): TypeFilterDropdownButtonState => {
+  switch (action.type) {
+    case 'set personal':
+      return { type: '개인' };
+    case 'set public':
+      return { type: '공용' };
+  }
+};
+
 export const TypeFilterDropdownButton: FC<TypeFilterDropdownButtonProps> = ({
-  onChange,
-  value = '종류',
   onApply,
 }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    type: null,
+  });
+
+  const handleClickPersonal = useCallback(() => {
+    dispatch({ type: 'set personal' });
+  },                                      []);
+  const handleClickPublic = useCallback(() => {
+    dispatch({ type: 'set public' });
+  },                                    []);
+
   return (
-    <DropdownButton buttonText={value} onClose={onApply}>
+    <DropdownButton
+      buttonText={state.type || '종류'}
+      onClose={onApply.bind(undefined, state.type)}
+    >
       <S.TypeButton
-        onClick={onChange.bind(null, '개인')}
-        isSelected={value === '개인'}
+        onClick={handleClickPersonal}
+        isSelected={state.type === '개인'}
       >
         개인
       </S.TypeButton>
       <S.TypeButton
-        onClick={onChange.bind(null, '공용')}
-        isSelected={value === '공용'}
+        onClick={handleClickPublic}
+        isSelected={state.type === '공용'}
       >
         공용
       </S.TypeButton>
