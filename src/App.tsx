8341d0c6c4e9450ffdde5633151d1238b9d0ce98
김형규 <k7120792@gmail.com>
2019-06-13 00:08:45 +0900

    Add redux and containers to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router';
 import { createBrowserHistory } from 'history';
-import { Signup, Signin } from './containers';
+import { createGlobalStyle } from 'styled-components';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+
+import { Signup, Signin } from './pages';
+
+import {
+  RegisterShareItemInfoContainer,
+  RegisterShareItemPeriodContainer,
+} from './containers';
 
 interface Props {}
 
-interface State {
-}
+interface State {}
 
 const history = createBrowserHistory();
+const GlobalStyle = createGlobalStyle`
+  div, input {
+    box-sizing: border-box;
+  }
+`;
+const store = configureStore();
 
 class App extends React.Component<Props, State> {
   render() {
     return (
-      <Router history={history}>
-        <Switch>
-          <Route exact path="/" component={Signup} />
-          <Route exact path="/signin" component={Signin} />
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router history={history}>
+          <GlobalStyle />
+          <Switch>
+            <Route exact path="/" component={Signup} />
+            <Route exact path="/signin" component={Signin} />
+            <Route
+              exact
+              path="/register/share/item"
+              component={RegisterShareItemInfoContainer}
+            />
+            <Route
+              exact
+              path="/register/share/period"
+              component={RegisterShareItemPeriodContainer}
+            />
+          </Switch>
+        </Router>
+      </Provider>
     );
   }
 }
