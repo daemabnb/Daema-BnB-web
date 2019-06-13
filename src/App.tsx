c1cf30bcbc493513b6d74bfcf18f292f522ffd48
김형규 <k7120792@gmail.com>
2019-06-13 10:11:30 +0900

    Modify containers to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router';
 import { createBrowserHistory } from 'history';
-import { Signup, Signin } from './containers';
+import { Signup, Signin } from './pages';
 
 interface Props {}
 
