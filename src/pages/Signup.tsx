dc76edf77b7935fe4e8428221b54a1bb40db4fbd
김형규 <k7120792@gmail.com>
2019-06-14 01:13:53 +0900

    Modify different code

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,29 +1,68 @@
 import React, { useState } from 'react';
 
 import { EmailChecker, LabeledTextInput, Button } from '../presentations';
+import { authMail, signup } from '../lib/user';
+import { getSessionStorage } from '../lib';
+import { History } from 'history';
 
 interface Props {
-  onSignup: () => void;
+  history: History;
 }
 
-export const Signup: React.FC = () => {
+export const Signup: React.FC<Props> = ({ history }) => {
   const [email, setEmail] = useState('');
   const [validation, setValidation] = useState('');
+  const [emailSended, setEmailSended] = useState(false);
+  const token = getSessionStorage('token');
+  const checkToken = () => {
+    if (!token) {
+      alert('facebook 로그인이 필요합니다.');
+      history.push('/signin');
+    }
+  };
+  checkToken();
+  const onAuthEmail = () => {
+    checkToken();
+    if (token) {
+      authMail(email, token)
+        .then(res => {
+          if (res.status === 201) {
+            setEmailSended(true);
+          }
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  };
+  const onSignup = () => {
+    checkToken();
+    if (token) {
+      signup(email, validation, token)
+        .then(res => {
+          if (res.status === 201) {
+            history.push('/');
+          }
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  };
   return (
     <div>
       <EmailChecker
+        disabled={emailSended}
         email={email}
         onChange={setEmail}
-        onCheck={() => {
-          console.log(1);
-        }}
+        onCheck={onAuthEmail}
       />
       <LabeledTextInput
         value={validation}
         onChange={setValidation}
         placeholder="인증번호"
       />
-      <Button content="가입하기" size="big" />
+      <Button content="가입하기" size="big" onClick={onSignup} />
     </div>
   );
 };
