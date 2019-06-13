85a84e0dc959578462d697e44f7ca8f8940afddb
김형규 <k7120792@gmail.com>
2019-06-13 17:15:24 +0900

    Add token to headers

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -2,16 +2,32 @@ import axios from 'axios';
 
 const baseUrl = 'https://52.79.121.254/user';
 
-export const authMail = (email: string) =>
-  axios.post(`${baseUrl}/authmail`, {
-    email,
-  });
+export const authMail = (email: string, token: string) =>
+  axios.post(
+    `${baseUrl}/authemail`,
+    {
+      email,
+    },
+    {
+      headers: {
+        token,
+      },
+    },
+  );
 
-export const signup = (email: string, authNum: string) =>
-  axios.post(`${baseUrl}/signup`, {
-    email,
-    authNum,
-  });
+export const signup = (email: string, authNum: string, token: string) =>
+  axios.post(
+    `${baseUrl}/signup`,
+    {
+      email,
+      authNum,
+    },
+    {
+      headers: {
+        token,
+      },
+    },
+  );
 
 export const signin = (token: string) =>
   axios.post(`${baseUrl}/signin/facebook`, { accessToken: token });
