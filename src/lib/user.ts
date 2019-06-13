3a931caf03652b9d6ab856c091bca034f16fe538
김형규 <k7120792@gmail.com>
2019-06-13 09:18:49 +0900

    Modify signin api

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -2,13 +2,16 @@ import axios from 'axios';
 
 const baseUrl = 'https://52.79.121.254/user';
 
-export const authMail = (email: string) => axios.post(`${baseUrl}/authmail`, {
-  email,
-});
+export const authMail = (email: string) =>
+  axios.post(`${baseUrl}/authmail`, {
+    email,
+  });
 
-export const signup = (email: string, authNum: string) => axios.post(`${baseUrl}/signup`, {
-  email,
-  authNum,
-});
+export const signup = (email: string, authNum: string) =>
+  axios.post(`${baseUrl}/signup`, {
+    email,
+    authNum,
+  });
 
-export const signin = () => axios.get(`${baseUrl}/signin/facebook`);
+export const signin = (token: string) =>
+  axios.post(`${baseUrl}/signin/facebook`, { accessToken: token });
