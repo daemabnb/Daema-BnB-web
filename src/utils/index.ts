c8497229a2791078d16ec31a21d235e4599ecb9b
김형규 <k7120792@gmail.com>
2019-06-13 10:06:33 +0900

    Add modify cookie functions to index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
 export * from './toString';
 export * from './formatDate';
 export * from './getNextpaginationIndex';
+export * from './getCookie';
+export * from './setCookie';
+export * from './deleteCookie';
