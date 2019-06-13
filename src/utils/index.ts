25f00fbdde6e21d175bc1b2cee921b415b27e6ce
김형규 <k7120792@gmail.com>
2019-06-13 23:35:55 +0900

    Delete cookie functions from index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,3 @@
 export * from './toString';
 export * from './formatDate';
 export * from './getNextpaginationIndex';
-export * from './getCookie';
-export * from './setCookie';
-export * from './deleteCookie';
