6e68d30b821871f6e1d095c1d013c7c5fdc8611f
김형규 <k7120792@gmail.com>
2019-06-13 19:40:29 +0900

    Add signup api

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 import { EmailChecker, LabeledTextInput, Button } from '../presentations';
+import { authMail, signup } from '../lib/user';
+import { getCookie } from '../utils';
 import { History } from 'history';
 
 interface Props {
@@ -10,21 +12,53 @@ interface Props {
 export const Signup: React.FC<Props> = ({ history }) => {
   const [email, setEmail] = useState('');
   const [validation, setValidation] = useState('');
+  const [emailSended, setEmailSended] = useState(false);
+  const token = getCookie('token');
+  const checkToken = () => {
+    if (!token) {
+      alert('facebook 로그인이 필요합니다.');
+      history.push('/signin');
+    }
+  };
+  checkToken();
+  const onAuthEmail = () => {
+    if (token) {
+      authMail(email, token)
+        .then(res => {
+          setEmailSended(true);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+    checkToken();
+  };
+  const onSignup = () => {
+    if (token) {
+      signup(email, validation, token)
+        .then(res => {
+          history.push('/');
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+    checkToken();
+  };
   return (
     <div>
       <EmailChecker
+        disabled={emailSended}
         email={email}
         onChange={setEmail}
-        onCheck={() => {
-          console.log(1);
-        }}
+        onCheck={onAuthEmail}
       />
       <LabeledTextInput
         value={validation}
         onChange={setValidation}
         placeholder="인증번호"
       />
-      <Button content="가입하기" size="big" />
+      <Button content="가입하기" size="big" onClick={onSignup} />
     </div>
   );
 };
