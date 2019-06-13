d0b50d6b8e1e0c5a689d6b27d4b821b68e0e0cca
김형규 <k7120792@gmail.com>
2019-06-13 19:38:00 +0900

    Add GlobalStyle to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router';
 import { createBrowserHistory } from 'history';
 import { Signup, Signin } from './pages';
+import { createGlobalStyle } from 'styled-components';
 
 interface Props {}
 
-interface State {
-}
+interface State {}
 
 const history = createBrowserHistory();
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
 
 class App extends React.Component<Props, State> {
   render() {
     return (
       <Router history={history}>
+        <GlobalStyle />
         <Switch>
-          <Route exact path="/" component={Signup} />
+          <Route exact path="/signup" component={Signup} />
           <Route exact path="/signin" component={Signin} />
         </Switch>
       </Router>
