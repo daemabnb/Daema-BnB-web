b0025a276f46a77f65cd62e3d20f60eb6cce8c9f
김형규 <k7120792@gmail.com>
2019-06-24 15:51:28 +0900

    Add User store

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import { RegistrationState, registerReducer as registration } from './register';
+import { UserState, userReducer as user } from './user';
 
 export interface StoreState {
   registration: RegistrationState;
+  user: UserState;
 }
 
 export default combineReducers<StoreState>({
   registration,
+  user,
 });
---
+++ src/store/modules/user.ts
@@ -0,0 +1,61 @@
+export interface UserState {
+  token: string;
+  isAdmin: boolean;
+}
+
+export const CHANGE_TOKEN = 'user/CHANGE_TOKEN';
+export const CHANGE_ADMIN_STATE = 'user/CHANGE_ADMIN_STATE';
+
+interface ChangeTokenAction {
+  type: typeof CHANGE_TOKEN;
+  token: string;
+}
+interface ChangeAdminStateAction {
+  type: typeof CHANGE_ADMIN_STATE;
+  isAdmin: boolean;
+}
+
+export type UserActionTypes = ChangeTokenAction | ChangeAdminStateAction;
+
+const changeToken = (token: string) => {
+  return {
+    type: CHANGE_TOKEN,
+    token,
+  };
+};
+const changeAdminState = (isAdmin: boolean) => {
+  return {
+    type: CHANGE_ADMIN_STATE,
+    isAdmin,
+  };
+};
+
+export const actionCreators = {
+  changeToken,
+  changeAdminState,
+};
+
+const initialState: UserState = {
+  token: '',
+  isAdmin: false,
+};
+
+export function userReducer(
+  state = initialState,
+  action: UserActionTypes,
+): UserState {
+  switch (action.type) {
+    case 'user/CHANGE_TOKEN':
+      return {
+        ...state,
+        token: action.token,
+      };
+    case 'user/CHANGE_ADMIN_STATE':
+      return {
+        ...state,
+        isAdmin: action.isAdmin,
+      };
+    default:
+      return state;
+  }
+}
