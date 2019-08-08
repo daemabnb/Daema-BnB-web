594102974509f0911d7997c82957b51dd7c0c83f
Kim Hyunggyu <k7120792@gmail.com>
2019-08-08 14:40:06 +0900

    Modify props name

--- src/presentations/EmailChecker/index.tsx
+++ src/presentations/EmailChecker/index.tsx
@@ -6,20 +6,20 @@ import * as S from './styles';
 interface Props {
   disabled?: boolean;
   email: string;
-  onChange: (email: string) => void;
+  onChangeEmail: (email: string) => void;
   onCheck: () => void;
 }
 
 export const EmailChecker: React.FC<Props> = ({
   email,
-  onChange,
+  onChangeEmail,
   onCheck,
   disabled = false,
 }) => {
   return (
     <S.EmailChecker>
       <LabeledTextInput
-        onChange={onChange}
+        onChange={onChangeEmail}
         value={email}
         placeholder="이메일 주소"
         disabled={disabled}
